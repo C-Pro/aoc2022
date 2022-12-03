bb798e58c3a9b659a8f5a3a8122c450403161f09
Sergey Melekhin <sergey@melekhin.me>
2022-12-03 12:45:24 +0700

    day 2

--- 03/1.py
+++ 03/1.py
@@ -1,5 +1,20 @@
 with open("input.txt", "rt") as fi:
-    lines=fi.read().splitlines()
-    a = [int(l) for l in lines]
+    lines = fi.read().splitlines()
 
-print(a)
+s = 0
+for l in lines:
+    mid = len(l) // 2
+    first = l[:mid]
+    second = l[mid:]
+    assert (len(first) == len(second))
+    wrong = set(first).intersection(set(second))
+    assert (len(wrong) == 1)
+    wrong = list(wrong)[0]
+    if wrong.islower():
+        priority = ord(wrong)-ord('a')+1
+    else:
+        priority = ord(wrong)-ord('A')+27
+    print(wrong, priority)
+    s += priority
+
+print(s)
