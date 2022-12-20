0adf493e029d087df0714f8cba2c161911a46e42
Sergey Melekhin <sergey@melekhin.me>
2022-12-20 10:18:06 +0700

    x

--- 16/2.py
+++ 16/2.py
@@ -1,5 +1,6 @@
 import itertools
 import re
+from math import inf
 from collections import deque
 
 
@@ -11,7 +12,7 @@ f = {}
 m = {}
 mem = {}
 
-with open("test.txt", "rt") as fi:
+with open("input.txt", "rt") as fi:
     lines = fi.read().splitlines()
     for line in lines:
         if line == "":
@@ -22,13 +23,55 @@ with open("test.txt", "rt") as fi:
         m[v] = [x.strip() for x in line[line.index("valve")+6:].split(",")]
 
 mem = {}
+dists = {}
+
+
+def dj(s, e):
+    unvisited = set(m.keys())
+    dists = {p: inf for p in unvisited}
+    dists[s] = 0
+    curr = s
+    while len(unvisited) > 0:
+        for p in m[curr]:
+            if dists[p] > dists[curr] + 1:
+                dists[p] = dists[curr] + 1
+        unvisited.remove(curr)
+        if len(unvisited) == 0:
+            return dists
+        if min([dists[p] for p in unvisited]) == inf:
+            print("unreachable nodes detected")
+            return dists
+        curr = sorted([(p, d) for p, d in dists.items()
+                      if p in unvisited], key=lambda x: x[1])[0][0]
+    return dists
+
+
+def gkey(s, e):
+    "distance key for non directional graph"
+    return tuple(sorted([s, e]))
+
+
+for s in f.keys():
+    for e in f.keys():
+        if e == s:
+            continue
+        dists.update({gkey(s, e): v for k, v in dj(s, e).items()})
 
 
 def dfs(c1, c2, cnt1, cnt2, o):
     if cnt1 + cnt2 < 2:
         return 0
 
-    print(c1, c2, cnt1, cnt2)
+    #print(c1, c2, cnt1, cnt2)
+    closed = set(f.keys()) - set(o)
+
+    try:
+        if cnt1 < min([dists[gkey(c1, c)] for c in closed if c != c1]) + 2 and \
+                cnt2 < min([dists[gkey(c2, c)] for c in closed if c != c2]) + 2:
+            return 0
+    except:
+        None
+
     ms = 0
     for (h, e) in itertools.product(*[[True, False], [True, False]]):
         cntI1 = cnt1
--- 19/1.py
+++ 19/1.py
@@ -3,10 +3,10 @@ from collections import deque
 
 
 def getints(line):
-    return [int(s) for s in re.findall("[0-9]+", line)]
+    return [int(s) for s in re.findall("-?[0-9]+", line)]
 
 
-with open("input.txt", "rt") as fi:
+with open("test.txt", "rt") as fi:
     lines = fi.read().splitlines()
     a = [getints(l) for l in lines]
 
