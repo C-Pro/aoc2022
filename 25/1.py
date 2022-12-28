7a3e4c0d0cec996c71a1a1bcead9d1a97e7b5d27
Sergey Melekhin <sergey@melekhin.me>
2022-12-28 16:34:43 +0700

    day 25 pt1

--- 25/1.py
+++ 25/1.py
@@ -1,23 +1,37 @@
-import re
-from collections import deque
-
 digits = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+stigid = {v: k for k, v in digits.items()}
+
 
 def fs(line):
-    return sum([digits[c]*(5**i) for i,c in enumerate(reversed(line))])
+    return sum([digits[c]*(5**i) for i, c in enumerate(reversed(line))])
 
-def ts(num):
-    s = []
-    while num > 0:
-        s.append(num % 5)
 
 assert (fs("1=-0-2") == 1747)
 assert (fs("12111") == 906)
 assert (fs("21") == 11)
 
 
-with open("test.txt", "rt") as fi:
+def ts(num):
+    s = ""
+    um = 0
+    while num != 0:
+        dig = num % 5
+        if dig > 2:
+            um = 1
+            dig = dig - 5
+        s += stigid[dig]
+        num = num // 5 + um
+        um = 0
+
+    return "".join(reversed(s))
+
+
+assert (ts(1747) == "1=-0-2")
+assert (ts(906) == "12111")
+assert (ts(11) == "21")
+
+with open("input.txt", "rt") as fi:
     lines = fi.read().splitlines()
 
 s = sum([fs(line) for line in lines])
-print(s)
+print(ts(s))
